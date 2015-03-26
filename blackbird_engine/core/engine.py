bbe3ab8d38e89fa72f02c735eac5a98ec4db2ce6
Loren McGinnis <mcginnis.loren@gmail.com>
2015-03-26 07:36:57 -0600

    renamed mock file

---
+++ blackbird_engine/core/engine.py
@@ -0,0 +1,13 @@
+class Engine:
+    def process_interview(self, portal_msg):
+        model = portal_msg['M']
+        question = portal_msg['Q']
+        response = portal_msg['R']
+
+        pass
+
+    def get_forecast(self, portal_model, fixed, ask):
+        pass
+
+    def get_landscape_summary(self, portal_model):
+        pass
