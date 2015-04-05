83d1e0f74ecce8d3e12c2a849d073452f7168396
Loren McGinnis <mcginnis.loren@gmail.com>
2015-04-04 21:09:08 -0600

    mock blackbird engine

---
+++ blackbird_engine/migrations/0001_initial.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import json_field.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='BlackbirdModel',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created_timestamp', models.DateTimeField(auto_now_add=True)),
+                ('complete', models.BooleanField(default=False)),
+                ('user_context', json_field.fields.JSONField(default=dict, help_text='Enter a valid JSON object')),
+                ('industry', models.CharField(null=True, max_length=256)),
+                ('summary',
+                 json_field.fields.JSONField(default='null', help_text='Enter a valid JSON object', null=True)),
+                ('business_name', models.CharField(null=True, max_length=256)),
+                ('tags', json_field.fields.JSONField(default='null', help_text='Enter a valid JSON object', null=True)),
+                ('e_model', json_field.fields.JSONField(default='null', help_text='Enter a valid JSON object')),
+            ],
+            options={
+                'ordering': ('-business', '-complete', '-created_timestamp'),
+            },
+        ),
+        migrations.CreateModel(
+            name='Business',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created_timestamp', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created_timestamp', models.DateTimeField(auto_now_add=True)),
+                ('sequence_num', models.PositiveSmallIntegerField(default=0)),
+                ('valid', models.BooleanField(default=True)),
+                ('e_question',
+                 json_field.fields.JSONField(default='null', help_text='Enter a valid JSON object', null=True)),
+                ('question_id', models.CharField(null=True, max_length=64)),
+                ('topic_name', models.CharField(null=True, max_length=64)),
+                ('progress', models.FloatField(default=0.0)),
+                ('short', models.CharField(null=True, max_length=64)),
+                ('prompt', models.TextField(null=True)),
+                ('comment', models.TextField(null=True)),
+                ('array_caption', models.TextField(null=True)),
+                ('input_array', json_field.fields.JSONField(default=list, help_text='Enter a valid JSON object')),
+                ('input_type', models.CharField(default='text', max_length=64)),
+                ('input_sub_type', models.CharField(null=True, max_length=64)),
+                ('transcribe', models.BooleanField(default=False)),
+                ('response_array',
+                 json_field.fields.JSONField(default='null', help_text='Enter a valid JSON object', null=True)),
+                (
+                'blackbird_model', models.OneToOneField(to='blackbird_engine.BlackbirdModel', related_name='question')),
+                ('business', models.ForeignKey(to='blackbird_engine.Business', related_name='questions')),
+            ],
+            options={
+                'ordering': ('business', 'valid', 'sequence_num'),
+            },
+        ),
+        migrations.AddField(
+            model_name='blackbirdmodel',
+            name='business',
+            field=models.ForeignKey(to='blackbird_engine.Business', related_name='blackbird_models'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='question',
+            unique_together=set([('business', 'valid', 'sequence_num')]),
+        ),
+        migrations.AlterIndexTogether(
+            name='question',
+            index_together=set([('business', 'valid', 'transcribe', 'sequence_num')]),
+        ),
+        migrations.AlterIndexTogether(
+            name='blackbirdmodel',
+            index_together=set([('business', 'complete', 'created_timestamp')]),
+        ),
+    ]
---
+++ blackbird_engine/settings/base.py
@@ -0,0 +1,64 @@
+"""
+Django settings for blackbird_web project.
+"""
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+##################################################################
+# Security settings
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.setdefault('DJANGO_SECRET_KEY', '^7+sgz#-hmzhx#h^e#hunx+6s##eiv%^npc&h$g)ca)+0o#11x')
+
+hosts = os.environ.setdefault('DJANGO_ALLOWED_HOSTS', '')
+ALLOWED_HOSTS = hosts.split(sep=',') if hosts else []
+
+##################################################################
+# Application definition
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+    'django.contrib.messages',
+    'rest_framework',
+    'json_field',
+    'blackbird_engine',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+##################################################################
+# Url & wsgi configuration
+ROOT_URLCONF = 'blackbird_engine.urls'
+WSGI_APPLICATION = 'blackbird_engine.wsgi.application'
+
+##################################################################
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+##################################################################
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
+
+
+##################################################################
+# Environment specific:
+# Database settings (https://docs.djangoproject.com/en/1.7/ref/settings/#databases)
+#   Debug settings
+#   Email settings
+#   Logging settings
