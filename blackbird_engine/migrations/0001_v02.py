49e4463a14422d143dd493849f4979bfa98062f7
Loren McGinnis <mcginnis.loren@gmail.com>
2015-04-26 00:12:14 -0600

    consolidated migrations

--- blackbird_engine/migrations/0001_v02.py
+++ blackbird_engine/migrations/0001_v02.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import blackbird_engine.models
 import json_field.fields
 
 
@@ -14,16 +15,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='BlackbirdModel',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('created_timestamp', models.DateTimeField(auto_now_add=True)),
                 ('complete', models.BooleanField(default=False)),
-                ('user_context', json_field.fields.JSONField(default=dict, help_text='Enter a valid JSON object')),
+                ('user_context', json_field.fields.JSONField(default=dict, help_text='Enter a valid JSON object', blank=True)),
                 ('industry', models.CharField(null=True, max_length=256)),
-                ('summary',
-                 json_field.fields.JSONField(default='null', help_text='Enter a valid JSON object', null=True)),
+                ('summary', json_field.fields.JSONField(default='null', null=True, help_text='Enter a valid JSON object')),
                 ('business_name', models.CharField(null=True, max_length=256)),
-                ('tags', json_field.fields.JSONField(default='null', help_text='Enter a valid JSON object', null=True)),
-                ('e_model', json_field.fields.JSONField(default='null', help_text='Enter a valid JSON object')),
+                ('tags', json_field.fields.JSONField(default='null', null=True, help_text='Enter a valid JSON object')),
+                ('e_model', models.FileField(upload_to=blackbird_engine.models.model_filename, null=True)),
             ],
             options={
                 'ordering': ('-business', '-complete', '-created_timestamp'),
@@ -32,19 +32,18 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Business',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('created_timestamp', models.DateTimeField(auto_now_add=True)),
             ],
         ),
         migrations.CreateModel(
             name='Question',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('created_timestamp', models.DateTimeField(auto_now_add=True)),
                 ('sequence_num', models.PositiveSmallIntegerField(default=0)),
                 ('valid', models.BooleanField(default=True)),
-                ('e_question',
-                 json_field.fields.JSONField(default='null', help_text='Enter a valid JSON object', null=True)),
+                ('e_question', json_field.fields.JSONField(default='null', null=True, help_text='Enter a valid JSON object')),
                 ('question_id', models.CharField(null=True, max_length=64)),
                 ('topic_name', models.CharField(null=True, max_length=64)),
                 ('progress', models.FloatField(default=0.0)),
@@ -56,11 +55,9 @@ class Migration(migrations.Migration):
                 ('input_type', models.CharField(default='text', max_length=64)),
                 ('input_sub_type', models.CharField(null=True, max_length=64)),
                 ('transcribe', models.BooleanField(default=False)),
-                ('response_array',
-                 json_field.fields.JSONField(default='null', help_text='Enter a valid JSON object', null=True)),
-                (
-                'blackbird_model', models.OneToOneField(to='blackbird_engine.BlackbirdModel', related_name='question')),
-                ('business', models.ForeignKey(to='blackbird_engine.Business', related_name='questions')),
+                ('response_array', json_field.fields.JSONField(default='null', null=True, help_text='Enter a valid JSON object')),
+                ('blackbird_model', models.OneToOneField(to='blackbird_engine.BlackbirdModel', related_name='question')),
+                ('business', models.ForeignKey(related_name='questions', to='blackbird_engine.Business')),
             ],
             options={
                 'ordering': ('business', 'valid', 'sequence_num'),
@@ -69,11 +66,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='blackbirdmodel',
             name='business',
-            field=models.ForeignKey(to='blackbird_engine.Business', related_name='blackbird_models'),
-        ),
-        migrations.AlterUniqueTogether(
-            name='question',
-            unique_together=set([('business', 'valid', 'sequence_num')]),
+            field=models.ForeignKey(related_name='blackbird_models', to='blackbird_engine.Business'),
         ),
         migrations.AlterIndexTogether(
             name='question',
