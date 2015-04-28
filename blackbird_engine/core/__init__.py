c303582ada69512511c6dba9e0f0363a88617fcd
Ilya Podolyako <ilya@blackbirdcreditmarket.com>
2015-04-28 17:57:35 -0400

    Added path adapter for core Engine modules.

--- blackbird_engine/core/__init__.py
+++ blackbird_engine/core/__init__.py
@@ -1,1 +1,18 @@
+
+
+
+
+#imports
+import os
+import sys
+
+
+#globals
 __author__ = 'loren'
+
+#IOP: Adding adapter for Engine paths that assume core is the root dir
+sub_dir = r"blackbird_engine/core"
+full_path = os.path.abspath(sub_dir)
+#abspath should control for system style (fwd vs back slash, etc.) differences
+sys.path.append(full_path)
+#all core modules should now be able to reference each other as before
