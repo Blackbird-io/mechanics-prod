8a7ea2624c7f09ee231146ade1a80fa50a212407
Ilya Podolyako <ilya@blackbirdcreditmarket.com>
2015-04-28 22:29:50 -0400

    Tweaking the path adapter more. Beanstalk found Shell but not SimplePortal last time, may be this will help.

--- blackbird_engine/core/__init__.py
+++ blackbird_engine/core/__init__.py
@@ -15,5 +15,6 @@ sub_dir = r"blackbird_engine/core"
 sub_dir = os.path.normpath(sub_dir)
 full_path = os.path.abspath(sub_dir)
 #abspath should control for system style (fwd vs back slash, etc.) differences
-sys.path.append(full_path)
+sys.path.append(sub_dir)
+##sys.path.append(full_path)
 #all core modules should now be able to reference each other as before
