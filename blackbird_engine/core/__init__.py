1131e862cc96bc6102a07614ac2b940410d430ab
Ilya Podolyako <ilya@blackbirdcreditmarket.com>
2015-04-28 21:51:43 -0400

    Added path normalization for path adapter.

--- blackbird_engine/core/__init__.py
+++ blackbird_engine/core/__init__.py
@@ -12,6 +12,7 @@ __author__ = 'loren'
 
 #IOP: Adding adapter for Engine paths that assume core is the root dir
 sub_dir = r"blackbird_engine/core"
+sub_dir = os.path.normpath(sub_dir)
 full_path = os.path.abspath(sub_dir)
 #abspath should control for system style (fwd vs back slash, etc.) differences
 sys.path.append(full_path)
