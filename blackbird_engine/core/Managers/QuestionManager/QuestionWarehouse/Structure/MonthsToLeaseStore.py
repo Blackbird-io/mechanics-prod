22a6083eec8b9bfe4fd7072ecba3ab3cc67fa5e5
Ilya Podolyako <ilya@blackbirdcreditmarket.com>
2015-06-05 18:29:34 -0400

    Retail growth topics in progress.

--- blackbird_engine/core/Managers/QuestionManager/QuestionWarehouse/Structure/MonthsToLeaseStore.py
+++ blackbird_engine/core/Managers/QuestionManager/QuestionWarehouse/Structure/MonthsToLeaseStore.py
@@ -4,18 +4,20 @@
 #NOT TO BE CIRCULATED OR REPRODUCED WITHOUT PRIOR WRITTEN APPROVAL OF ILYA PODOLYAKO
 
 #Blackbird Environment
-#Module: QW.Structure.TimeToBuildStore
+#Module: QW.Structure.MonthsToLeaseStore
 """
 
-Module asks question about time to find the right property and lease a store. 
+Module asks question about time to find the right property and lease a store.
+
+See Blackbird Portal-Engine API Guide for attribute explanations.
 ====================  ==========================================================
 Attribute             Description
 ====================  ==========================================================
 
 DATA:
-active_elements
-array_caption
-basic_prompt
+active_elements       
+array_caption         
+basic_prompt          
 comment
 custom_prompt
 input_type
@@ -35,7 +37,7 @@ n/a
 ====================  ==========================================================
 """
 question_content = True
-name = "time to open leased store?"
+name = "months to sign a new store lease?"
 question_author = "Ilya Podolyako"
 
 #Engine parameters
@@ -46,16 +48,15 @@ optionalTags = []
 #Portal parameters
 #
 array_caption = None
-comment = "The pre-opening period covers all construction, permitting, hiring, "
-comment += "and marketing activities necessary for the doors to open. You "
-comment += "should include a bit of cushion (think of when you can reasonably"
-comment += "promise to deliver a store)."
+comment = "If tomorrow you decided to open a new store, you would need
+comment += "this many months to find the right property and get all the "
+comment += "paperwork in order."
 input_type = "number"
 input_sub_type = "months"
-basic_prompt = "Once you sign a lease, how many months do you usually need to"
-basic_prompt += "open a new store for business?"
+basic_prompt = "How many months do you usually spend looking before you sign"
+basic_prompt += " a new store lease?"
 custom_prompt = None
-short = "Pre-Opening"
+short = "Property Search"
 transcribe = True
 active_elements = 1
 
