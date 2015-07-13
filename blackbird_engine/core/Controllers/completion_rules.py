2cb3bafd9ab24c84a34a30bc741a38ba61942f62
Ilya Podolyako <ilya@blackbirdcreditmarket.com>
2015-07-13 10:48:32 -0400

    Build passes some tests, fails soft_4, shows odd behavior on zero teams.

--- blackbird_engine/core/Controllers/completion_rules.py
+++ blackbird_engine/core/Controllers/completion_rules.py
@@ -46,17 +46,19 @@ def check_quality_only(item):
 
 
     Function returns True iff:
-    (1) item quality is at or above item's own quality standard.
+    (1) item quality is at or above item's own quality standard, or
+    (2) item finished topic with catalog with no matches.
 
     Function marks complete = True on objects that pass and False on those that
     fail.
     """
     complete = False
-    #
-    standard = item.guide.quality.standard
-    if item.guide.quality.current >= standard:
+    if item.guide.selection.finished_catalog:
         complete = True
-    #
+    if not complete:
+        standard = item.guide.quality.standard
+        if item.guide.quality.current >= standard:
+            complete = True    
     item.guide.complete = complete
     #
     return complete
@@ -69,28 +71,30 @@ def check_quality_or_attention(item):
 
 
     Function returns True iff:
-    (1) item quality is at or above item's own quality standard, or
-    (2) item attention spend is at or above item's own attention budget.
+    (1) item quality is at or above item's own quality standard,
+    (2) item attention spend is at or above item's own attention budget, or
+    (3) item finished topic with catalog with no matches.
 
     Function marks complete = True on objects that pass and False on those that
     fail.
     """
     complete = False
-    #
-    quality_cap = item.guide.quality.standard
-    attention_cap = item.guide.attention.budget
-    #
-    worked_out = False
-    asked_out = False
-    #
-    if item.guide.quality.current >= quality_cap:
-        worked_out = True
-    if attention_cap:
-        if item.guide.attention.current >= attention_cap:
-            asked_out = True
-    #
-    if any(worked_out, asked_out):
+    if item.guide.selection.finished_catalog:
         complete = True
+    if not complete:
+        quality_cap = item.guide.quality.standard
+        attention_cap = item.guide.attention.budget
+        #
+        worked_out = False
+        asked_out = False
+        #
+        if item.guide.quality.current >= quality_cap:
+            worked_out = True
+        if attention_cap:
+            if item.guide.attention.current >= attention_cap:
+                asked_out = True
+        if any(worked_out, asked_out):
+            complete = True
     #
     item.guide.complete = complete
     #
