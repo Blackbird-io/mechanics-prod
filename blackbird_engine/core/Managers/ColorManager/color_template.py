9b25bbe59fbd1335cf2fb7225f514efdd2d33e1b
Ilya Podolyako <ilya@blackbirdcreditmarket.com>
2015-08-26 18:37:36 -0400

    working on turning market color into a catalog.

---
+++ blackbird_engine/core/Managers/ColorManager/color_template.py
@@ -0,0 +1,21 @@
+#market color template
+
+#imports
+from DataStructures.X.X import MarketColor
+
+#globals
+market_color_content = True
+color = MarketColor(author = "IOP", date = "2015-08-24")
+
+#retail
+color.add_industry("retail")
+#adds a standard industry_color dict to color["retail"]
+color["retail"]["blah"] = blah
+color["retail"]["blah2"] = blah2
+
+#software
+
+#industry_3
+#industry_4
+
+
