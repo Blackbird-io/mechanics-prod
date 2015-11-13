96167f2650290677dd6e39df63c71cfde3d101cf
Ilya Podolyako <ilya@blackbirdcreditmarket.com>
2015-11-13 13:31:54 -0500

    Added common events, cleaned up BusinessUnit method names, moved some to non-public.

---
+++ blackbird_engine/core/data_structures/modelling/common_events.py
@@ -0,0 +1,44 @@
+#PROPRIETARY AND CONFIDENTIAL
+#Property of Blackbird Logical Applications, LLC
+#Copyright Blackbird Logical Applications, LLC 2015
+#NOT TO BE CIRCULATED OR REPRODUCED WITHOUT PRIOR WRITTEN APPROVAL OF ILYA PODOLYAKO
+
+#Blackbird Environment
+#Module: data_structures.modelling.common_events
+"""
+
+Flat module, contains strings that name common business events. By using these
+keys, you maximize the probability that the engine recognizes your event and
+acts appropriately.
+====================  ==========================================================
+Attribute             Description
+====================  ==========================================================
+
+DATA:
+KEY_KILLED            shutting a unit down ahead of schedule
+KEY_RENOVATION        investing in a unit to extend its life
+
+FUNCTIONS:
+n/a
+
+CLASSES:
+n/a
+====================  ==========================================================
+"""
+
+
+
+# Imports
+# n/a
+
+
+
+
+# Constants
+KEY_KILLED = "killed"
+KEY_RENOVATION = "renovation"
+
+# Classes
+# n/a
+
+# Should not have any logic here. 
