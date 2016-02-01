c76dabdcee88bd4d47980e7fe2ec217e97c0a5e4
Ilya Podolyako <ilya@blackbirdcreditmarket.com>
2016-02-01 18:58:47 -0500

    Working on implementing modular relationship-to-link logic.

--- blackbird_engine/core/data_structures/modelling/_xl_mgmt.py
+++ blackbird_engine/core/data_structures/modelling/_xl_mgmt.py
@@ -1,6 +1,6 @@
 #Data structures for managing excel interface
 
-class PositionTracker:
+class Range:
     """
 
     Uses absolute coordinates
@@ -10,16 +10,16 @@ class PositionTracker:
         
         self.starting = starting
         self.ending = None
-        self.active = None
+        self.data = list() 
         # points to active row or column
 
-class PositionLookup(PositionTracker):
+class Lookup(Range):
     """
-    Uses position relative to starting point
+    values in the lookup are always relative to the starting point
     """
 
     def __init__(self, *pargs, **kwargs):
-        PositionTracker.__init__(self, *pargs, **kwargs)
+        Range.__init__(self, *pargs, **kwargs)
         self.by_name = dict()
 
     def get_position(self, name):
@@ -29,21 +29,16 @@ class PositionLookup(PositionTracker):
     def update(self, source):
         pass
 
-class RangeLookup:
+class Area:
     """
+    could also name this ``Field``
     """
-    
     def __init__(self):
-
-        self.rows = PositionLookup()
-        self.cols = PositionLookup()
-
-    def update(self, source):
-        # update both rows and cols by name
-        pass
-
         
-class SplitPosition(PositionTracker):
+        self.rows = Lookup()
+        self.cols = Lookup()
+        
+class LineData(PositionTracker):
 
     """
     For coordinating line location across multiple non-continguous
@@ -52,18 +47,66 @@ class SplitPosition(PositionTracker):
 
     def __init__(self):
 
-        PositionTracker.__init__(self)
+        Range.__init__(self)
 
-        self.consolidated = PositionTracker()
-        self.consolidated.cells = []
-        # A 1-dim array of cells where we store values
-        self.derived = PositionTracker()
-        self.details = PositionTracker()
+        self.consolidated = Range()
+        # Expects each item in consolidated.data to be a row_data object
+        self.derived = Range()
+        # Expects each item in derived.data to be a driver_data object
+        self.detailed = Range()
+        # detailed.data should be empty?
 
-class XL:
+    def get_coordinates(self, column):
+        """
+        -> tuple(int, int)
+
+        x (column), y (row)
+
+        # should return a dictionry of "sheet":, "row":, "column":
+        """
+        x = column
+        y = self.ending
+
+        result = (x, y)
+        return result
+        # Each line must be able to deliver a full coordinate set, with
+        # sheet. Could add a ._set_sheet() method that goes through
+        # each line and sets the sheet pointer. Or could do something with
+        # like a shared pointer, though that's uglier. Let's just have
+        # a function. When we get the right sheet for that unit we, we
+        # _set_sheet() and then we can derive. Or can pass it in during the
+        # call somehow. But that requires the coordinator to know where to look
+
+class UnitData:
+    def __init__(self, book=None, sheet=None):
+        self.sheet = sheet
+        
+class RowData:
     def __init__(self):
-        self.location = LineManager()
-        self.book = None
-        self.in_progress = True
+        dict.__init__(self)
+        self["value"] = None
+        self["label"] = None
+        self["comment"] = None
+        self["references"] = None
+
+class DriverData:
+    """
+    rows : list of RowData objects
+    references : dictionary of strings to objects, suite
+    """
+    def __init__(self):
+        self.rows = []
+        self.formula_string = None
+        self.formula_references = None
+        self.conversion_map = None
+        
+             
+
+        
+
+
+    
+
+        
 
     
