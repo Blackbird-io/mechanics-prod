05f51739343f73a3e238c276c4d5014093c1e8d3
Ilya Podolyako <ilya@blackbirdcreditmarket.com>
2016-02-02 11:09:26 -0500

    Moved excel work into dedicated ``serializers`` data_structures module. Starting to test in runtime.

---
+++ blackbird_engine/core/data_structures/serializers/__init__.py
@@ -0,0 +1,37 @@
+#PROPRIETARY AND CONFIDENTIAL
+#Property of Blackbird Logical Applications LLC. Copyright 2016
+#NOT TO BE CIRCULATED OR REPRODUCED WITHOUT PRIOR WRITTEN APPROVAL
+    
+
+        
+
+        
+
+
+
+             
+        
+         
+        
+            
+    
+
+
+        
+        
+
+                
+                
+            
+        
+        
+        
+
+
+
+
+
+        
+
+       
+    
