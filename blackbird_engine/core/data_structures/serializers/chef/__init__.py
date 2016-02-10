9dc7b9a7edb97b4363f47cee1aee06fe0e39732f
Ilya Podolyako <ilya@blackbirdcreditmarket.com>
2016-02-10 17:14:56 -0500

    Batch-based consolidation works.

--- blackbird_engine/core/data_structures/serializers/chef/__init__.py
+++ blackbird_engine/core/data_structures/serializers/chef/__init__.py
@@ -3,16 +3,6 @@
 #NOT TO BE CIRCULATED OR REPRODUCED WITHOUT PRIOR WRITTEN APPROVAL
     
 
-
-
-##def chop(self, *pargs, model):
-##    from .model_chef import ModelChef
-##    gordon = Chef()
-##    result = gordon.chop_model(model)
-##    return result
-##    # should make ModelChef.chop() a class method
-
-
         
 
         
