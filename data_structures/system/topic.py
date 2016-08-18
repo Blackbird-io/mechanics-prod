8b219686b68d4c7af1e6de61ef003a5a9420138f
Dmitry Hindanov <dmitry@blackbird.io>
2016-08-18 10:18:30 -0400

    Case 1226 cleanup

--- data_structures/system/topic.py
+++ data_structures/system/topic.py
@@ -2,6 +2,7 @@
 # Property of Blackbird Logical Applications, LLC
 # Copyright Blackbird Logical Applications, LLC 2016
 # NOT TO BE CIRCULATED OR REPRODUCED WITHOUT PRIOR WRITTEN APPROVAL
+
 # Blackbird Environment
 # Module: data_structures.system.topic
 """
