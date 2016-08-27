fd5c257b35ae7584ba3055560a41ed0297ba2bca
Erika Granger <erika@bbcredit.io>
2016-08-27 19:06:30 -0400

    Case 1304: works!

--- data_structures/modelling/financials.py
+++ data_structures/modelling/financials.py
@@ -253,6 +253,7 @@ class Financials:
         new_instance = Financials()
         new_instance._full_order = self._full_order.copy()
         new_instance._compute_order = self._compute_order.copy()
+        new_instance._exclude_statements = self._exclude_statements.copy()
 
         for name in self.full_order:
             own_statement = getattr(self, name)
