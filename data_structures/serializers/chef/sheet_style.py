64592a0421750ddcbb2e7953c707c53f003dab7a
Erika Granger <erika@bbcredit.io>
2016-10-06 14:58:42 -0400

    Case 1483: updated SheetStyle to contain only static methods and use other static classes appropriately

--- data_structures/serializers/chef/sheet_style.py
+++ data_structures/serializers/chef/sheet_style.py
@@ -4,7 +4,7 @@
 # NOT TO BE CIRCULATED OR REPRODUCED WITHOUT PRIOR WRITTEN APPROVAL
 
 # Blackbird Environment
-# Module: data_structures.serializers.chef.cell_styles
+# Module: data_structures.serializers.chef.sheet_style
 """
 
 Module defines a class that stores standard row and column names.
@@ -41,8 +41,6 @@ SHOW_GRID_LINES = False
 ZOOM_SCALE = 80
 
 # Module Globals
-
-field_names = FieldNames()
 get_column_letter = xlio.utils.get_column_letter
 
 # Classes
@@ -58,31 +56,25 @@ class SheetStyle:
     n/a
 
     FUNCTIONS:
-    n/a
+    label_areas()         method labels all Areas on the sheet
+    set_column_width()    method sets the column width for provided column
+    style_sheet()         method formats sheet view, sets zoom, turns off lines
     ====================  =====================================================
     """
-
-    def style_sheet(self, sheet, label_areas=True):
-        """
-
-
-        SheetStyle.style_sheet() -> None
-
-        --``sheet`` is an openpyxl worksheet
-        --``label_areas`` whether or not to label areas, default is true
-
-        Method labels areas on sheet (label_areas == True) and implements
-        Blackbird sheet styling.
-        """
-        if label_areas:
-            self.label_areas(sheet)
-
-        sheet.sheet_view.showGridLines = SHOW_GRID_LINES
-        sheet.sheet_view.zoomScale = ZOOM_SCALE
-
     @staticmethod
     def label_areas(sheet):
-        areas_exclude = set((field_names.GENERAL, field_names.TIMELINE))
+        """
+
+
+        SheetStyle.label_areas() -> None
+
+        --``sheet`` is an openpyxl worksheet
+
+        Method iterates over all Areas attached to sheet.bb and labels them.
+        Method excludes the General and Timeline areas which are standard on
+        many sheets but do not get labeled by default.
+        """
+        areas_exclude = set((FieldNames.GENERAL, FieldNames.TIMELINE))
         areas = set(sheet.bb.area_names)
         areas = areas - areas_exclude
 
@@ -96,5 +88,35 @@ class SheetStyle:
 
     @staticmethod
     def set_column_width(sheet, column, width=COLUMN_WIDTH):
+        """
+
+
+        SheetStyle.set_column_width() -> None
+
+        --``sheet`` is an openpyxl worksheet
+        --``column`` is the number of the column you which to format
+        --``width`` is the width to set on the column
+
+        Method sets the Excel column width the for column.
+        """
         column = sheet.column_dimensions[get_column_letter(column)]
         column.width = width
+
+    @staticmethod
+    def style_sheet(sheet, label_areas=True):
+        """
+
+
+        SheetStyle.style_sheet() -> None
+
+        --``sheet`` is an openpyxl worksheet
+        --``label_areas`` whether or not to label areas, default is true
+
+        Method labels areas on sheet (label_areas == True) and implements
+        Blackbird sheet styling.
+        """
+        if label_areas:
+            SheetStyle.label_areas(sheet)
+
+        sheet.sheet_view.showGridLines = SHOW_GRID_LINES
+        sheet.sheet_view.zoomScale = ZOOM_SCALE
