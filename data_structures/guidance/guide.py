321190dfdcce8cd44e2f60815354b0ba5313df7a
Dmitry Hindanov <dmitry@blackbird.io>
2016-10-20 17:08:50 -0400

    Case 1461 noop

--- data_structures/guidance/guide.py
+++ data_structures/guidance/guide.py
@@ -1,10 +1,10 @@
-#PROPRIETARY AND CONFIDENTIAL
-#Property of Blackbird Logical Applications, LLC
-#Copyright Blackbird Logical Applications, LLC 2014
-#NOT TO BE CIRCULATED OR REPRODUCED WITHOUT PRIOR WRITTEN APPROVAL OF ILYA PODOLYAKO
+# PROPRIETARY AND CONFIDENTIAL
+# Property of Blackbird Logical Applications, LLC
+# Copyright Blackbird Logical Applications, LLC 2016
+# NOT TO BE CIRCULATED OR REPRODUCED WITHOUT PRIOR WRITTEN APPROVAL
 
-#Blackbird Environment
-#Module: data_structures.guidance.guide
+# Blackbird Environment
+# Module: data_structures.guidance.guide
 
 """
 This module defines the Guide class, which provides a standard gauge cluster for
@@ -26,7 +26,7 @@ Guide                 gauge cluster that guides analysis
 
 
 
-#imports
+# imports
 from .attention_tracker import AttentionTracker
 from .counter import Counter
 from .quality_tracker import QualityTracker
@@ -35,10 +35,10 @@ from .selection_tracker import SelectionTracker
 
 
 
-#globals
-#n/a
+# globals
+# n/a
 
-#classes
+# classes
 class Guide:
     """
 
@@ -50,27 +50,27 @@ class Guide:
 
     DATA:
     HIGHEST_PERMITTED_PRIORITY  int; default of 5
-    
+
     attention             instance of AttentionTracker
     complete              bool; convenience marker used by external objects
     quality               instance of QualityTracker
     priority              instance of Counter
     selection             instance of SelectionTracker
-    
-    
+
+
     FUNCTIONS:
     reset()               resets state attributes of instance
     ====================  ======================================================
     """
     HIGHEST_PERMITTED_PRIORITY = 5
-    
+
     def __init__(self, priority=None, quality=None):
-        
+
         self.attention = AttentionTracker()
         self.complete = False
 
         self.priority = Counter(cut_off=self.HIGHEST_PERMITTED_PRIORITY)
-        
+
         self.quality = QualityTracker()
         self.selection = SelectionTracker()
         if priority:
