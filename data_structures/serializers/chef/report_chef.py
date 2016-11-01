b38792126e925cd07203978f57d8ac500c24ee6e
Erika Granger <erika@bbcredit.io>
2016-10-31 23:19:30 -0400

    Case 1535: updated doc strings

--- data_structures/serializers/chef/report_chef.py
+++ data_structures/serializers/chef/report_chef.py
@@ -28,12 +28,13 @@ ReportChef            class to generate reports from forecast and actual values
 
 # Imports
 from data_structures.modelling.line_item import LineItem
+from openpyxl.styles.colors import WHITE, BLACK
 from openpyxl.utils import get_column_letter
 
 from .cell_styles import CellStyles
+from .data_types import TypeCodes, NumberFormats
 from .formulas import FormulaTemplates
 from .sheet_style import SheetStyle
-from .data_types import TypeCodes, NumberFormats
 
 
 
@@ -56,15 +57,15 @@ class ReportChef:
     ==============  ===========================================================
 
     DATA:
+    actual          obj; instance of Timeline containing actual financials
     end_date        datetime.date; last date for which to make reports
+    forecast        obj; instance of Timeline containing forecast financials
     model           obj; instance of blackbird model
-    spec_dates      bool; make reports for all available dates (False) or 
-                    specific dates (True)
-    st_date         datetime.date; first date for which to make reports
+    start_date      datetime.date; first date for which to make reports
 
     FUNCTIONS:
-    build_reports()  method builds reports within date range
     add_report()     method adds a specific report
+    build_reports()  method builds reports within date range
     ===============  ==========================================================
     """
 
@@ -74,51 +75,36 @@ class ReportChef:
         self.actual = actl
 
         if dates is not None:
-            self.spec_dates = True
             self.start_date = min(dates)
             self.end_date = max(dates)
         else:
-            self.spec_dates = False
             self.start_date = min(self.actual.keys())
             self.end_date = max(self.actual.keys())
 
         self._placeholder = '--'  # value to use in reports for Divide by Zero 
                                  # error or unavailable data
 
-    def build_reports(self, wb):
-        """
-        At this point, the "Forecast" and "Actual" tabs will already exist,
-        so we can access cell coordinates.
-
-        actual_periods = sorted(actual.values(), keyed by .start)
-        for act_period in actual_periods:
-            if act_period.start >= self.start_date and act_period.end <= self.end_date:
-                for_period = forecast.find_period(actual_period.end)
-
-                self.add_report(wb, act_period, for_period)
-
-
-            elif act_period.end > self.end_date:
-                break # since we sorted keys first, we know we won't want
-                      # any future periods
-
-        """
-        actual_periods = sorted(self.actual.values(), key=lambda x: x.start)
-
-        for act_period in actual_periods:
-            if act_period.start >= self.start_date and act_period.end <= self.end_date:
-                for_period = self.forecast.find_period(act_period.end)
-
-                self.add_report(wb, act_period, for_period)
-
-            elif act_period.end > self.end_date:
-                # since we sorted keys first, we know we won't want
-                # any future periods
-                break
+        # FORMATTING
+        self._report_tab_color = WHITE
+        self._banner_color = '002060'  # navy blue
+        self._banner_font_color = WHITE
+        self._tbl_hdr_color = '00FFFFFF'  # transparent
+        self._tbl_hdr_font_color = BLACK
 
     def add_report(self, wb, act_period, for_period):
         """
-        lasdkjflkajds
+
+
+        ReportChef.add_report() -> None
+
+        --``wb`` must be a BB_Workbook
+        --``act_period`` must be a TimePeriod object containing actual reported
+                         financials
+        --``for_period`` must be a TimePeriod object containing forecast
+                         financials corresponding to those in ``act_period``
+
+        Method creates a new report tab and fills in the forecast and actual
+        data.  Method adds columns for delta and percent difference.
         """
         # Do preliminary formatting
         use_date = act_period.end
@@ -155,11 +141,101 @@ class ReportChef:
 
         CellStyles.format_border_group(sheet, st_col, ed_col, st_row, ed_row)
 
+    def build_reports(self, wb):
+        """
+
+
+        ReportChef.build_reports() -> None
+
+        --``wb`` must be a BB_Workbook
+
+        Method delegates to add_report() to build reports within the time
+        range specified on instantiation (between self.start_date and
+        self.end_date).
+        """
+        actual_periods = sorted(self.actual.values(), key=lambda x: x.start)
+
+        for act_period in actual_periods:
+            if act_period.start >= self.start_date and act_period.end <= self.end_date:
+                for_period = self.forecast.find_period(act_period.end)
+
+                self.add_report(wb, act_period, for_period)
+
+            elif act_period.end > self.end_date:
+                # since we sorted keys first, we know we won't want
+                # any future periods
+                break
+
+    # ************************************************************************#
+    #                          NON-PUBLIC METHODS                             #
+    # ************************************************************************#
+
+    def _add_details(self, sheet, act_line, for_line, row_container, indent=0):
+        """
+        Method adds details within report format.  Method formulates a
+        summation equation which will be set as the value for the parent
+        line to show this simple relationship.
+        """
+        all_cols = sheet.bb.col_axis.get_group('all')
+        forecast_col = all_cols.get_group('Forecast')
+        actual_col = all_cols.get_group('Actual')
+
+        actual_details = act_line.get_ordered()
+
+        if actual_details:
+            sub_indent = indent + LineItem.TAB_WIDTH
+            act_detail_summation = ""
+            for_detail_summation = ""
+
+            for act_det in actual_details:
+                for_det = for_line.find_first(act_det.name)
+
+                self._report_line(
+                    sheet=sheet,
+                    act_line=act_det,
+                    for_line=for_det,
+                    row_container=row_container,
+                    indent=sub_indent,
+                )
+
+                link_template = FormulaTemplates.ADD_COORDINATES
+
+                cos = act_det.xl.get_coordinates(include_sheet=False)
+                link = link_template.format(coordinates=cos)
+                act_detail_summation += link
+
+                cos = for_det.xl.get_coordinates(include_sheet=False)
+                link = link_template.format(coordinates=cos)
+                for_detail_summation += link
+
+            row_container.calc_size()
+
+            if act_line.xl.format.blank_row_before:
+                row_container.add_group('spacer_details', size=1)
+
+            # subtotal row for details
+            line_label = indent * " " + act_line.title
+            finish = row_container.add_group(act_line.title, size=1) # , label=line_label
+
+            sheet.bb.calc_sizes()
+
+            # ACTUAL
+            act_cell = sheet.cell(column=actual_col.number(), row=finish.number())
+            act_cell.set_explicit_value(act_detail_summation, data_type=TypeCodes.FORMULA)
+            act_line.xl.cell = act_cell
+            CellStyles.format_line(act_line)
+
+            # FORECAST
+            for_cell = sheet.cell(column=forecast_col.number(), row=finish.number())
+            for_cell.set_explicit_value(for_detail_summation, data_type=TypeCodes.FORMULA)
+            for_line.xl.cell = for_cell
+            CellStyles.format_line(for_line)
+
     def _make_report_header(self, sheet, title, date):
         """
-        Add header
+        Method formats header portion of report tabs.
         """
-        sheet.sheet_properties.tabColor = 'FFFFFF'
+        sheet.sheet_properties.tabColor = self._report_tab_color
         SheetStyle.style_sheet(sheet)
 
         all_rows = sheet.bb.row_axis.add_group('all', offset=1)
@@ -187,7 +263,8 @@ class ReportChef:
         banner_cell = sheet.cell(row=ban_row.number(), column=label_col.number())
         banner_cell.value = title
         CellStyles.format_header_label(banner_cell, alignment='center',
-                                       color='002060', font_color='FFFFFF',
+                                       color=self._banner_color,
+                                       font_color=self._banner_font_color,
                                        bold=True, font_size=16, name='Arial')
 
         # merge banner cells
@@ -214,8 +291,8 @@ class ReportChef:
             cell = sheet.cell(row=header_row.number(), column=col.number())
             cell.value = col.name
             CellStyles.format_header_label(cell, alignment='center',
-                                           color='00FFFFFF',
-                                           font_color="000000")
+                                           color=self._tbl_hdr_color,
+                                           font_color=self._tbl_hdr_font_color)
             CellStyles.format_border(cell, bottom=True)
 
         # Set Column Widths
@@ -224,42 +301,12 @@ class ReportChef:
         for c in range(label_col.number(), perc_diff.number()+1):
             SheetStyle.set_column_width(sheet, c, 16.63)
 
-    def _report_statement(self, sheet, act_statement, for_statement, name=None):
-        """
-        Add report statements
-        """
-        all_cols = sheet.bb.col_axis.get_group('all')
-        lab_col = all_cols.get_group('labels')
-
-        all_rows = sheet.bb.row_axis.get_group('all')
-
-        if name is None:
-            name = act_statement.name
-
-        stat_rows = all_rows.add_group(name, offset=1)
-
-        lab_row = stat_rows.add_group('label', size=1)
-        sheet.bb.calc_sizes()
-
-        cell = sheet.cell(row=lab_row.number(), column=lab_col.number())
-        cell.value = name.title()
-        CellStyles.format_bold(cell)
-
-        for act_line in act_statement.get_ordered():
-            for_line = for_statement.find_first(act_line.name)
-
-            self._report_line(sheet, act_line, for_line, stat_rows)
-
-            sheet.bb.calc_sizes()
-
     def _report_line(self, sheet, act_line, for_line, row_container, indent=0):
         """
-
-            - print line name
-            - set reference to Forecast cell
-            - set reference to Actual cell
-            - set formula for Delta cell (Actual cell - Forecast cell)
-            - set formula for Percent Difference cell (=IFERROR((Actual Cell - Forecast Cell )/ Forecast Cell, self.placeholder)
+        Method adds a specific LineItem to the report.  Method preserves
+        original cell designation on the ``Forecast`` or ``Actual`` tab.
+        Method adds links to source data for Forecast and Actual columns and
+        inserts formulas to calculate Delta and Percent Difference.
         """
         act_save_cell = act_line.xl.cell
         for_save_cell = for_line.xl.cell
@@ -361,71 +408,31 @@ class ReportChef:
         act_line.xl.cell = act_save_cell
         for_line.xl.cell = for_save_cell
 
-    def _add_details(self, sheet, act_line, for_line, row_container, indent=0):
+    def _report_statement(self, sheet, act_statement, for_statement, name=None):
         """
-
-
-        LineChef._add_detail() -> Worksheet
-
-        --``sheet`` must be an instance of openpyxl Worksheet
-        --``line`` must be an instance of LineItem
-        --``row_container`` coordinate anchor on the row axis
-        --``column`` must be a column number reference
-        --``indent`` is amount of indent
-
-        Displays detail sources.
+        Method adds a financial statement to the report, delegating to
+        the _report_line() method for most of the work.
         """
         all_cols = sheet.bb.col_axis.get_group('all')
-        forecast_col = all_cols.get_group('Forecast')
-        actual_col = all_cols.get_group('Actual')
+        lab_col = all_cols.get_group('labels')
 
-        actual_details = act_line.get_ordered()
+        all_rows = sheet.bb.row_axis.get_group('all')
 
-        if actual_details:
-            sub_indent = indent + LineItem.TAB_WIDTH
-            act_detail_summation = ""
-            for_detail_summation = ""
+        if name is None:
+            name = act_statement.name
 
-            for act_det in actual_details:
-                for_det = for_line.find_first(act_det.name)
+        stat_rows = all_rows.add_group(name, offset=1)
 
-                self._report_line(
-                    sheet=sheet,
-                    act_line=act_det,
-                    for_line=for_det,
-                    row_container=row_container,
-                    indent=sub_indent,
-                )
+        lab_row = stat_rows.add_group('label', size=1)
+        sheet.bb.calc_sizes()
 
-                link_template = FormulaTemplates.ADD_COORDINATES
+        cell = sheet.cell(row=lab_row.number(), column=lab_col.number())
+        cell.value = name.title()
+        CellStyles.format_bold(cell)
 
-                cos = act_det.xl.get_coordinates(include_sheet=False)
-                link = link_template.format(coordinates=cos)
-                act_detail_summation += link
+        for act_line in act_statement.get_ordered():
+            for_line = for_statement.find_first(act_line.name)
 
-                cos = for_det.xl.get_coordinates(include_sheet=False)
-                link = link_template.format(coordinates=cos)
-                for_detail_summation += link
-
-            row_container.calc_size()
-
-            if act_line.xl.format.blank_row_before:
-                row_container.add_group('spacer_details', size=1)
-
-            # subtotal row for details
-            line_label = indent * " " + act_line.title
-            finish = row_container.add_group(act_line.title, size=1) # , label=line_label
+            self._report_line(sheet, act_line, for_line, stat_rows)
 
             sheet.bb.calc_sizes()
-
-            # ACTUAL
-            act_cell = sheet.cell(column=actual_col.number(), row=finish.number())
-            act_cell.set_explicit_value(act_detail_summation, data_type=TypeCodes.FORMULA)
-            act_line.xl.cell = act_cell
-            CellStyles.format_line(act_line)
-
-            # FORECAST
-            for_cell = sheet.cell(column=forecast_col.number(), row=finish.number())
-            for_cell.set_explicit_value(for_detail_summation, data_type=TypeCodes.FORMULA)
-            for_line.xl.cell = for_cell
-            CellStyles.format_line(for_line)
