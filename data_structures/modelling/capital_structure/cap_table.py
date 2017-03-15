829364955ac9a22a76ab8830afb0ee260ab56272
Peter Hua <peter@blackbird.io>
2017-03-15 11:56:10 -0400

    Case 1871. Changed as_of_date to ref_date. Add get_last_snapshot() function

--- data_structures/modelling/capital_structure/cap_table.py
+++ data_structures/modelling/capital_structure/cap_table.py
@@ -27,6 +27,7 @@ CapTable              storage for capital structure information
 
 import bb_exceptions
 from collections import OrderedDict
+from tools.parsing import date_from_iso
 from datetime import date
 from data_structures.system.bbid import ID
 
@@ -49,6 +50,7 @@ class CapTable:
     FUNCTIONS:
     add_round()           adds a Round to self.rounds
     add_snapshot()        adds a Snapshot to self.snapshots
+    get_last_snapshot()   returns most recent Snapshot
     ====================  ======================================================
     """
     def __init__(self):
@@ -92,7 +94,7 @@ class CapTable:
         If overwrite is False, method checks that the snapshot does not conflict
         with existing entries before adding the new Snapshot.
         """
-        key = new_snapshot.as_of_date
+        key = new_snapshot.ref_date
 
         # Make sure this snapshot is valid
         if not overwrite:
@@ -102,4 +104,35 @@ class CapTable:
                 raise bb_exceptions.BBAnalyticalError(c)
 
         self.snapshots[key] = new_snapshot
+
+    def get_last_snapshot(self, ref_date=None):
+        """
+
+
+        CapTable.get_last_snapshot() -> Snapshot
+
+        --``ref_date`` is datetime date, or string "YYYY-MM-DD"
+
+        Method returns the most recent Snapshot on or before a certain date.
+        Date defaults to today() if not specified
+        """
+        if not ref_date:
+            ref_date = date.today()
+        elif isinstance(ref_date, str):
+            ref_date = date_from_iso(ref_date)
+
+        last_key = None
+        for key in self.snapshots:
+            if key <= ref_date:
+                if not last_key:
+                    last_key = key
+                elif key > last_key:
+                    last_key = key
+
+        if last_key:
+            return self.snapshots[last_key]
+        else:
+            return None
+            # c = "No Snapshots exist before %s" % date
+            # raise bb_exceptions.BBAnalyticalError(c)
 
