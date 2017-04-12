5e921706d3f236df9aa196c36ae7a72533267688
Peter Hua <peter@blackbird.io>
2017-04-12 09:44:47 -0400

    Case 1912. Add parsing functionality as a seperate module in tools. Exactly the same as what was originally in the topic

---
+++ tools/xl_parsing/__init__.py
@@ -0,0 +1,9 @@
+# PROPRIETARY AND CONFIDENTIAL
+# Property of Blackbird Logical Applications, LLC
+# Copyright Blackbird Logical Applications, LLC 2016
+# NOT TO BE CIRCULATED OR REPRODUCED WITHOUT PRIOR WRITTEN APPROVAL
+# Blackbird Environment
+# Module: data_structures.modelling.xl_parser.__init__
+"""
+Package that contains objects that Blackbird uses to analyze capital structure.
+"""
