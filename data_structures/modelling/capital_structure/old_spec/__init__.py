6146ccf98bd93e7f0abf378d84622abf9d39b454
Erika Granger <erika@bbcredit.io>
2017-04-20 11:26:29 -0400

    Case 1866: deleted some obsolete files

--- data_structures/modelling/capital_structure/old_spec/__init__.py
+++
@@ -1,8 +0,0 @@
-# PROPRIETARY AND CONFIDENTIAL
-# Property of Blackbird Logical Applications, LLC
-# Copyright Blackbird Logical Applications, LLC 2016
-# NOT TO BE CIRCULATED OR REPRODUCED WITHOUT PRIOR WRITTEN APPROVAL
-# Blackbird Environment
-# Module: data_structures.modelling.old_spec.__init__
-"""
-"""
